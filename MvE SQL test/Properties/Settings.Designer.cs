6baeb01cccc329b9185caaa53502a3ba92bf3067
Machiel van Emden <machielve@gmail.com>
2023-11-28 22:23:57 +0100

    Rename

--- MvE SQL test/Properties/Settings.Designer.cs
+++ MvE SQL test/Properties/Settings.Designer.cs
@@ -8,34 +8,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Project_pancake.Properties
-{
-
-
+namespace Project_Pancake_client.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
             }
         }
-
+        
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("server=192.168.2.61;user id=machiel;persistsecurityinfo=True;database=Victoriam;p" +
             "assword=Welkom#2017")]
-        public string connString
-        {
-            get
-            {
+        public string connString {
+            get {
                 return ((string)(this["connString"]));
             }
         }
